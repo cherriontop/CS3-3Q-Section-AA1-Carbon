f9b7aeb39837f4921427836662e46d899573e24a
𐙚₊˚⊹ rish <reacarbon22@src.pshs.edu.ph>
2025-01-22 15:33:56 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const pi = 3.14;
+var r = 6.378e6; 
+var c = 2*pi*r;
+var s = 4*pi*r**2;
+var v = 4/3*pi*r**3;
+
+document.write('The value radius of the sphere is ' + r);
+document.write('<br><br>');
+document.write('The circumference is ' + c);
+document.write('<br><br>');
+document.write('The surface area is ' + s);
+document.write('<br><br>');
+document.write('The volume is ' + v);
