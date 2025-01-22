dea96971aeee1a3044ac6a3ace86b22b66ce8065
𐙚₊˚⊹ rish <reacarbon22@src.pshs.edu.ph>
2025-01-22 15:36:36 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const pi = 3.14;
-var r = 6.378e6; 
-var c = 2*pi*r;
-var s = 4*pi*r**2;
-var v = 4/3*pi*r**3;
+var radius = 6.378e6; 
+var circumference = 2 * pi * radius;
+var surfaceArea = 4 * pi * radius ** 2;
+var volume = (4 / 3) * pi * radius ** 3;
 
-document.write('The value radius of the sphere is ' + r);
+document.write('The radius of the sphere is ' + radius + '.');
 document.write('<br><br>');
-document.write('The circumference is ' + c);
+document.write('The circumference is ' + circumference + '.');
 document.write('<br><br>');
-document.write('The surface area is ' + s);
+document.write('The surface area is ' + surfaceArea + '.');
 document.write('<br><br>');
-document.write('The volume is ' + v);
+document.write('The volume is ' + volume + '.');
+
